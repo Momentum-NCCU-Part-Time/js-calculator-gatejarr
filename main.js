9f2026e280fc8956b0607571200a4e3ee778f479
Jarrod <gatejarr@gmail.com>
2023-10-16 20:01:02 -0400

    still trying to get the calculator to show answers

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 let outputWindow = document.getElementById('output');
 let numbersOperatorsArray = document.querySelectorAll('.numberButton, .operationButton');
-let clearButton = document.getElementById('clearButton');
 let calculate = document.getElementById('equalsButton');
-
 
 for (let button of numbersOperatorsArray) {
   button.addEventListener('click', (target) => outputWindow.innerText += button.innerText);
@@ -10,29 +8,13 @@ for (let button of numbersOperatorsArray) {
 
 calculate.addEventListener('click', (target) => {
   let answer = eval(outputWindow.innerText);
+  outputWindow.innerText = answer.innerText;
   console.log (answer);
 }
 )
 
+document.getElementById('clearButton').addEventListener('click', function() {
+  location.reload();
+}
+)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// for (let el of clearButton) {
-//   el.addEventListener ('click', (target) => outputWindow.innerText = "");
-// }
