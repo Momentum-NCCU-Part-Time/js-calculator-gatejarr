663da8749ea7abd00f08305c856cbc7b8744b076
Jarrod <gatejarr@gmail.com>
2023-10-16 23:21:56 -0400

    Got my C button to act as a DEL button, but it deletes numbers from answers as well.

--- main.js
+++ main.js
@@ -3,21 +3,12 @@ let numbersOperatorsArray = document.querySelectorAll('.numberButton, .operation
 let calculate = document.getElementById('equalsButton');
 let newOperation = false;
 
-// for (let button of numbersOperatorsArray) {
-//   button.addEventListener('click', (target) => outputWindow.innerText += button.innerText);
-// }
-
+//  TO-DO
 //	figure out how limit output window to 22 numbers and operations
 //	figure out how to start a new calculation when a number button is clicked after equals,
 //	but continue current calculation if an operator button is clicked
 
-calculate.addEventListener('click', (target) => {
-  let answer = eval(outputWindow.innerText.replace("x", "*"));
-  outputWindow.innerText = answer;
-  newOperation = true;
-  console.log (answer);
-}
-)
+//Output Window code & code to start new operation
 for (let button of numbersOperatorsArray) {
   button.addEventListener('click', (target) => {
     if (button.classList.contains('numberButton') && newOperation)
@@ -30,8 +21,25 @@ for (let button of numbersOperatorsArray) {
     }
   })
 }
-// document.getElementById('clearButton').addEventListener('click', function())
 
+//  Calculations code, still won't keep current calc going with an operator button click
+//  Starts new calc when number button clicked after equals button
+//  Replaces "x" character with asterisk (*) to force multiplication operation
+calculate.addEventListener('click', (target) => {
+  let answer = eval(outputWindow.innerText.replace("x", "*"));
+  outputWindow.innerText = answer;
+  newOperation = true;
+  console.log (answer);
+}
+)
+
+//  Clear Button code
+document.getElementById('clearButton').addEventListener('click', function(){
+  outputWindow.innerText = outputWindow.innerText.slice(0, -1);
+}
+)
+
+//  All Clear Button code
 document.getElementById('allClearButton').addEventListener('click', function() {
   location.reload();
 }
